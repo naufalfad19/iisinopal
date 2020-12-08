89eaca3b0a65f360227ccbbffe94e592e55efaff
naufalfad19 <62590450+naufalfad19@users.noreply.github.com>
2020-12-08 22:55:09 +0700

    Update connect.php

--- souvenir_bkt/admin/inc/connect.php
+++ souvenir_bkt/admin/inc/connect.php
@@ -1,8 +1,8 @@
 <?php
-	$host = "localhost";
-	$user = "postgres";
-	$pass = "opal191299";
+	$host = "ec2-34-195-169-25.compute-1.amazonaws.com";
+	$user = "vastjiccyrujng";
+	$pass = "65353e0e14733f71ac8fae19d9277f10f1dceef650f215fd18cd2cfc91273181";
 	$port = "5432";
-	$dbname = "souvenir";
+	$dbname = "d5gl0aajitlv3u";
 	$conn = pg_connect("host=".$host." port=".$port." dbname=".$dbname." user=".$user." password=".$pass) or die("Gagal");
 ?>
